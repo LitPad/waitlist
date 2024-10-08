0362be6e4e7730e55f4f808e031b7a1dbf57340d
Edobor <edoborobiaderi@gmail.com>
2024-10-08 23:12:39 +0100

    UI fixes on the modal and Hero section

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,12 @@ import { Input } from "./components/ui/input";
 import axios from "axios";
 import SocialLink from "./components/shared/SocialLink";
 import WelcomeLitpadModal from "./components/shared/WelcomeLitpadModal";
+import HeroSection from "./components/shared/HeroSection";
 
 function App() {
   const baseURL = import.meta.env.VITE_API_URL;
 
-  const [openModal, setOpenModal] = useState(false);
+  const [openModal, setOpenModal] = useState(true);
   const [loading, setLoading] = useState(false);
   const [updatingWaitList, setUpdatingWaitList] = useState(false);
   const [version, setVersion] = useState(0);
@@ -101,36 +102,7 @@ function App() {
           </div>
         </header>
         {/* hero */}
-        <div className="border border-[#F6F5F6] py-[60px] px-[15px] lg:p-[90px] mt-[70px] lg:m-[50px] rounded-[36px] flex flex-col items-center">
-          <span className=" flex flex-wrap justify-center  items-center">
-            <span className="text-[26px] md:text-[40px] xl:text-[56px] font-[600] text-[#2B1744]">
-              Discover Captivating Stories
-            </span>
-            <div className="bg-purple-gradient rounded-full px-[17px] py-[5px] lg:px-[23px] lg:py-[7px] flex items-center mx-2 lg:mx-[16px]">
-              <img
-                src="/assets/icons/heroBook.svg"
-                className="h-[20px] lg:h-[36px]"
-                alt="pen "
-              />
-            </div>
-            <span className="text-[26px] md:text-[40px] xl:text-[56px] font-[600] text-[#2B1744] text-wrap text-center">
-              Across
-            </span>
-          </span>
-          <span className="text-[26px] text-center md:text-[40px] xl:text-[56px] font-[600] text-[#2B1744]">
-            Genres You Love
-          </span>
-          <p className="text-[#7F7589] text-[14px] lg:text-[18px] font-[500] mb-[24px] max-w-[550px] text-wrap text-center px-4 pt-10 ">
-            LitPad offers you unlimited access to an ever-growing library of
-            quality novels, all for one budget-friendly subscription
-          </p>
-          <button
-            onClick={scrollToJoinWaitlist}
-            className="bg-btn-purple-gradient rounded-full text-[#FFFFFF] text-[15px] font-[600] h-[46px] w-[135px] lg:h-[52px] lg:w-[136px] hover:bg-purple-600 mt-4 "
-          >
-            Join waitlist
-          </button>
-        </div>
+        <HeroSection onClick={() => setOpenModal(true)} />
       </section>
 
       {/* Cards */}
--- src/components/shared/ShareSocial.tsx
+++ src/components/shared/ShareSocial.tsx
@@ -1,17 +1,22 @@
 type ShareSocialProps = {
   text: string;
   img: string;
+  href: string;
 };
-const ShareSocial = ({ text, img }: ShareSocialProps) => {
+const ShareSocial = ({ text, img, href }: ShareSocialProps) => {
   return (
-    <div className="bg-white py-[14px] px-4 rounded-[12px] flex justify-center items-center gap-2">
-      <span className="text-[16px]">{text}</span>
+    <a
+      href={href}
+      target="_blank"
+      className="bg-white py-[14px] px-4 rounded-[12px] flex justify-center items-center gap-2"
+    >
+      <span className="text-[14px] md:text-[16px]">{text}</span>
       <img
         className="w-[18px] h-[18px]"
         src={`/assets/icons/${img}`}
         alt="ig"
       />
-    </div>
+    </a>
   );
 };
 
